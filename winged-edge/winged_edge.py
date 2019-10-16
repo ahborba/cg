fdff75760d8ca1b013af503a2076a9b0c0deadd8
Alexandre H. Borba <alexandrehborba@gmail.com>
2019-10-16 14:23:28 -0300

    vertex e index implementados

--- winged-edge/winged_edge.py
+++ winged-edge/winged_edge.py
@@ -19,11 +19,11 @@ def read_vertex():
     for linha in vertex_file:
         linha = linha.strip('\n')
         el = linha.split(',')
-        posicao = el[0:3]
+        posicao = list(map(int,el[0:3]))
         del(el[0:3])
         vertex_dict['v'+str(cont)] = [posicao, el]
         cont += 1
-    print(vertex_dict)
+    # print(vertex_dict)
     vertex_file.close()
 
 def read_edge():
@@ -36,7 +36,7 @@ def read_edge():
         del(el[0:4])
         edge_dict['e'+str(cont)] = [vertex, faces, el]
         cont += 1
-    print(edge_dict)
+    # print(edge_dict)
     edge_file.close()
 
 
@@ -47,13 +47,29 @@ def read_face():
         face = linha.split(',')
         face_dict['f'+str(cont)] = face
         cont += 1
-    print(face_dict)
+    # print(face_dict)
     face_file.close()
+
+
+def get_index_list():
+    index_list = []
+    for edge_a,edge_b,edge_c in list(face_dict.values()):
+        vertex_list=[]
+        for vertex in [edge_dict[edge_a][0],edge_dict[edge_b][0],edge_dict[edge_c][0]]:
+            for v in vertex:
+                if not (int(v.strip('v')) in vertex_list):
+                    vertex_list.append(int(v.strip('v')))
+        index_list.append(vertex_list)
+    return index_list
 
 def main():
     read_vertex()
     read_edge()
     read_face()
+    vertex_list =  [x[0] for x in list(vertex_dict.values())]
+    index_list = get_index_list()
+    print(index_list)
+    print(vertex_list)
 
 if __name__=="__main__":
     main()
