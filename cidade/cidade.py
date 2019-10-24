2d1a247e6968eeec91360572838a6ec273789839
Alexandre H. Borba <alexandrehborba@gmail.com>
2019-10-24 16:09:33 -0300

    deslocamento nos eixos

--- cidade/cidade.py
+++ cidade/cidade.py
@@ -27,6 +27,8 @@ class cidade:
         self.ang_x = 0
         self.ang_y = 0
         self.ang_z = 0
+        self.dx = 0
+        self.dy = 0
         self.init_glut()
         self.teste, self.print = False, False
 
@@ -38,7 +40,7 @@ class cidade:
         glutInitWindowSize(w, h)
         glutCreateWindow('Cidade')
         # glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA)
-        # glutIdleFunc(self.display)
+        glutIdleFunc(self.camera)
         glViewport(0, 0, w, h)
         # glEnable(GL_DEPTH_TEST)
         glDepthFunc(GL_LEQUAL)
@@ -51,31 +53,31 @@ class cidade:
         glutMotionFunc(self.motion)
         glutSpecialFunc(self.special)
         glutDisplayFunc(self.display)
-        glutWarpPointer(250,250)
+        glutWarpPointer(int(w/2),int(h/2))
         glutKeyboardFunc(self.keyboard)
         
 
     def keyboard(self, key, x, y):
         key = key.decode('utf8').lower()
+        direcao = False
         if key == chr(27):
             os.system('clear')
             sys.exit()
         elif key == 'w':
             self.z -= d
-            self.axis_x=False
-        elif key == 's':
-            self.z += d
-            self.axis_x=False
         elif key == 'a':
             self.x -= d
-            self.axis_x=True
+            self.dir_x-=d
+        elif key == 's':
+            self.z += d
         elif key == 'd':
             self.x += d
-            self.axis_x=True
-        self.camera(x,y)
+            self.dir_x +=d
+
+        self.camera(direcao)
 
     def special(self, key, x, y):
-        d = 0.5
+        d = 1
         if key== GLUT_KEY_LEFT:
             self.ang_y -= 0.05
             self.dir_x = sin(self.ang_y)
@@ -92,49 +94,51 @@ class cidade:
             self.x -= self.dir_x * d
             self.z -= self.dir_z * d
             self.y -= self.dir_y * d
-        self.camera(x, y)
-
-    
+        self.camera()
 
     def motion(self, x, y):
-        self.camera(x, y)
+        x = (w/2)-x
+        y = (h/2)-y
+        # esquerda
+        if (x - self.dx) > 0 :
+            self.ang_y -= 0.01
+            self.dir_x = sin(self.ang_y)
+            self.dir_z = -cos(self.ang_y)
+        else:
+            # direita
+            self.ang_y += 0.01
+            self.dir_x = sin(self.ang_y)
+            self.dir_z = -cos(self.ang_y)
+        self.dx = x
+        self.dy = y
+        # self.camera(x, y,True)
 
     def mouse(self, tp1, tp2, x, y):
         print("mouse_click: ", x, y, sep=" ", end="\n")
 
-    ''' x , y , z = posicao da camera
-        x+dir_x , y+dir_y , z+dir_z = direcao em que a camera está olhando
-        0 , 1 , 0 = não sei'''
-    def camera(self, x=0, y=0):
-        # global fov
-        self.dir_x = (w/2)-x
-        self.dir_y = (h/2)-y
-        # glEnable(GL_DEPTH_TEST)
+    def camera(self, x=0, y=0,girar_camera=False):
+        if girar_camera:
+            self.dir_x = x
+            self.dir_y = y
         glClear(GL_COLOR_BUFFER_BIT)
         glMatrixMode(GL_MODELVIEW)
         glLoadIdentity()
-        # gluLookAt(1, 1, 1, 0, 0, 0, 0, 1, 0)
-
-        gluPerspective(.5, 1080/720, 1, 100)
-        
-        # gluPerspective(1,1,1,1,1)
-        if self.axis_x:
-            gluLookAt(self.x, self.y ,self.z,self.dir_x,self.dir_y,0 , 0, 1, 0)
-        else:
-            gluLookAt(self.x, self.y ,self.z,self.dir_x,self.dir_y,0 , 1, 0, 0)
+        gluPerspective(0.5, 1080/720, 1, 100)
+        gluLookAt(self.x,0,self.z,self.dir_x,self.dir_y,self.z-0.1,0,1,0)
+        #            gluLookAt(self.x, self.y ,self.z,self.dir_x,self.dir_y,0 , 1, 0, 0)
+        # gluLookAt(self.x, self.y ,self.z,self.dir_x,self.dir_y,0 , 0, 1, 0)
         glutPostRedisplay()
         os.system('clear')
-        print('x: ', self.x, '\ny: ', self.y, '\nz: ', self.z,'\n\ndir x: ',self.dir_x,'\ndir y: ',self.dir_y,'\n\nfov: ',fov)
-
-        
-        # gluLookAt(self.x, self.y, self.z, self.x+self.dir_x,self.y+self.dir_y, self.dir_z+self.z, 0, 1, 0)
-    
+        print('camera:  ',self.x,self.y,self.z)
+        print('direcao:',self.dir_x,self.dir_y,self.dir_z)
+        # print('x: ', self.x, '\ny: ', self.y, '\nz: ', self.z,'\n\ndir x: ',self.dir_x,'\ndir y: ',self.dir_y,'\n\nfov: ',fov)
 
 
 
     def display(self):
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
-        self.cube()
+        self.cube1()
+        self.cube2()
         self.point()
         self.axis()
         glFlush()
@@ -147,7 +151,7 @@ class cidade:
             glutFullScreenToggle()
         self.camera()
         self.axis()
-        self.cube()
+        self.cube1()
         self.pontos['casa'] = []
         self.pontos['telhado'] = []
 
@@ -173,7 +177,7 @@ class cidade:
             glVertex3fv([0,0,250])
             glEnd()
 
-    def cube(self):
+    def cube1(self):
         vertex = ((-250, 250, -250), (250, 250, -250), (-250, -250, -250), (250, -250, -250),
                   (-250, 250, 250), (250, 250, 250), (-250, -250, 250), (250, -250, 250))
         edges = ((0, 1), (1, 3), (3, 2), (2, 0), (3, 7), (7, 5),
@@ -185,6 +189,20 @@ class cidade:
             for v in edge:
                 glVertex3fv(vertex[v])
         glEnd()
+
+    def cube2(self):
+        vertex = ((300, 600, 300), (600, 600, 300), (300, 300, 300), (600, 300, 300),
+                  (300, 600, 600), (600, 600, 600), (300, 300, 600), (600, 300, 600))
+        edges = ((0, 1), (1, 3), (3, 2), (2, 0), (3, 7), (7, 5),
+                 (5, 1), (5, 4), (4, 0), (4, 6), (6, 7), (6, 2))
+        glBegin(GL_LINES)
+        glColor3f(random.randint(0, 255), random.randint(
+            0, 255), random.randint(0, 255))
+        for edge in edges:
+            for v in edge:
+                glVertex3fv(vertex[v])
+        glEnd()
+
 
 if __name__ == '__main__':
     git = cidade()
